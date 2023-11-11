2819bda35e2da32c5b0ec8297839eac285c49c9f
HCA01553 <116924963+HCA01553@users.noreply.github.com>
2023-11-11 14:48:14 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    lcd1602.clear()
+    for (let index = 0; index < 24; index++) {
+        lcd1602.putNumber(
+        randint(0, 9),
+        randint(0, 15),
+        randint(0, 1)
+        )
+        basic.pause(200)
+    }
+})
 lcd1602.setAddress(
 lcd1602.I2C_ADDR.addr1
 )
@@ -8,12 +19,7 @@ lcd1602.putString(
 )
 lcd1602.set_LCD_Show(lcd1602.visibled.visible)
 lcd1602.set_backlight(lcd1602.on_off.on)
-basic.pause(2000)
+basic.pause(50000)
 basic.forever(function () {
-    lcd1602.putNumber(
-    randint(0, 9),
-    randint(0, 15),
-    randint(0, 1)
-    )
-    basic.pause(200)
+	
 })
